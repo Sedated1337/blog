128841f016f7dcb74da22f79a266e1b8d7a7093b
Vlad Podolyan <vredbi123@gmail.com>
2022-01-11 23:00:24 +0200

    update options

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_09_225051) do
+ActiveRecord::Schema.define(version: 2022_01_11_202817) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_01_09_225051) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,10 @@
 require "faker"
 
 5.times do
-  authors = Author.new(
+  Author.create(
     gender: Faker::Gender.binary_type,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     birthday: Faker::Date.birthday(min_age: 18, max_age: 99)
   )
-  authors.save
 end
